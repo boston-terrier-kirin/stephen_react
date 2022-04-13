ad7bdc55a1111d07c55370de239f43efc63a907d
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-13 15:05:02 +0900

    jsx/Section2-24

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 
 function App() {
-  const name = 'Kohei';
+  const name = 'ex: John Doe';
+
+  // Uncaught Error: Objects are not valid as a React child
+  // const name = { text: 'ex: John Doe' };
+
   return (
     <div>
-      <label class="label" for="name">
+      <label className="label" htmlFor="name">
         Enter name:
       </label>
+      <input id="name" type="text" placeholder={name} />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        Click Me!
+      </button>
     </div>
   );
 }
