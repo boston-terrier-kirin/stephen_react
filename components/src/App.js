83fd933af2aa2665e6c0a1dcd8dcb00d3fc4e9a3
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-13 17:06:24 +0900

    components/Section3-42

--- components/src/App.js
+++ components/src/App.js
@@ -1,27 +1,34 @@
 import faker from '@faker-js/faker';
+import ApprovalCard from './ApprovalCard';
 import CommentDetail from './CommentDetail';
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 6:00PM"
-        content="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00PM"
-        content="React is awesome"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 6:00PM"
-        content="Which do you like?"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          content="React is awesome"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 6:00PM"
+          content="Which do you like?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
