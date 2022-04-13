de94fe4aa5f9f1f6c3035f0c5a6ff3f08ea9c943
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-13 16:35:50 +0900

    components/Section3-38

--- components/src/App.js
+++ components/src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
 import faker from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
 
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post.</div>
-        </div>
-      </div>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 6:00PM"
+        content="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        content="React is awesome"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 6:00PM"
+        content="Which do you like?"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 }
