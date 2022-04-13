e6c4cd6d93d7a22b9cfcac650164bbfe0526231f
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-13 14:29:42 +0900

    jsx/Section2-16

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Kohei';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
     </div>
   );
 }
