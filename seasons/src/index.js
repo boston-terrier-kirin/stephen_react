9d17f1f77b1b6c189ff3a6bcd495ab07fd1756b8
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-14 17:17:59 +0900

    seasons/Section6-70

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
   // babelがやってくれるので、コンストラクタは不要。
@@ -43,7 +44,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
