3d38e0eedec80ee5727b56712cc469e0214c4ff4
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-14 13:41:42 +0900

    seasons/Section4-53

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
 
-  return <div>Hi There!</div>;
-};
+    return <div>Latitude: </div>;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
