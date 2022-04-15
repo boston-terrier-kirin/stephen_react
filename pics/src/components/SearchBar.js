9ec48f564845f22c4a220e86e0a58c187c1dbc7d
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-15 10:32:05 +0900

    pics/Section7-90 いつも起きるthis問題。 解決１：bindする。 解決２：onSubmitをアロー関数の中で呼び出すことで、thisが固定されるので、問題が解決できる。 解決３：onFormSubmit 自体をアロー関数にして、thisを固定する。

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,18 +3,37 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(event) {
-    event.preventDefault();
+  // 解決１:bindする
+  // constructor(props) {
+  //   super(props);
+  //   this.onFormSubmit = this.onFormSubmit.bind(this);
+  // }
 
-    // いつも起きる問題。この時、this は undefined になっている。
-    console.log(this);
+  // 解決２：onSubmitをアロー関数の中で呼び出すことで、thisが固定されるので、問題が解決できる。
+  // onFormSubmit(event) {
+  //   event.preventDefault();
+  //   // いつも起きる問題。この時、this は undefined になっている。
+  //   console.log(this);
+  //   console.log(this.state.term);
+  // }
+
+  // 解決３：onFormSubmit 自体をアロー関数にして、thisを固定する。
+  onFormSubmit = (event) => {
+    event.preventDefault();
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form
+          className="ui form"
+          // 解決２：onSubmitをアロー関数の中で呼び出すことで、thisが固定されるので、問題が解決できる。
+          // onSubmit={(event) => this.onFormSubmit(event)}
+
+          // 解決３：onFormSubmit 自体をアロー関数にして、thisを固定する。
+          onSubmit={this.onFormSubmit}
+        >
           <div className="field">
             <label>Image Search</label>
             <input
