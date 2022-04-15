6a9a392b91e0e7c8fa05008f009f10a600422991
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-15 12:37:23 +0900

    pics/Section9-104

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,9 @@
+const ImageList = (props) => {
+  const images = props.images.map((image) => (
+    <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
+  ));
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
