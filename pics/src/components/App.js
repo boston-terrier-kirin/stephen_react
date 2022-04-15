8cccad196c7ee6a05a31a95adb68b60632470ef3
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-15 11:16:06 +0900

    pics/Section8-95

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
+import { environment } from '../environment';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: `Client-ID ${environment.accessKey}`,
+      },
+    });
   }
 
   render() {
