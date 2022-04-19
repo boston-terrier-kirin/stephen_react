6edf011546140343f229b8de0745a8fecf44117f
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-20 05:14:10 +0900

    videos/Section11-134

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,7 +2,11 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => (
-    <VideoItem video={video} onVideoSelect={onVideoSelect} />
+    <VideoItem
+      key={video.id.videoId}
+      video={video}
+      onVideoSelect={onVideoSelect}
+    />
   ));
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
