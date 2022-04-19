6a156ccfe2ddc7b90aa7bf7180da7e3af1922884
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-20 06:34:36 +0900

    widgets/Section12-140

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
 
-function App() {
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is frontend javascript framework',
+  },
+  {
+    title: 'Why React',
+    content: 'React is favorite framework',
+  },
+  {
+    title: 'How do you use React',
+    content: 'I dont know',
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
-}
+};
 
 export default App;
