f08d3ccf32c0a9641da511d9bfa3671f930baf4e
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-22 07:30:38 +0900

    blog/Section19-271

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,30 +3,34 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // export const fetchPosts = () => {
 //   return async (dispatch, getState) => {
 //     const res = await jsonPlaceholder.get('/posts');
-
+//
 //     dispatch({
 //       type: 'FETCH_POST',
 //       payload: res,
 //     });
 //   };
 // };
-
+//  ↓
+//  ↓
+//  ↓
 // export const fetchPosts = () => {
 //   return async (dispatch) => {
 //     const res = await jsonPlaceholder.get('/posts');
-
+//
 //     dispatch({
 //       type: 'FETCH_POST',
 //       payload: res,
 //     });
 //   };
 // };
-
+//  ↓
+//  ↓
+//  ↓
 export const fetchPosts = () => async (dispatch) => {
   const res = await jsonPlaceholder.get('/posts');
 
   dispatch({
-    type: 'FETCH_POST',
-    payload: res,
+    type: 'FETCH_POSTS',
+    payload: res.data,
   });
 };
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,8 +8,16 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return {
+    // state.posts の posts はcombineReducersで決めた名前。
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
