e263eb1872d692d414d8a6e437e3aca56e47f4a2
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-22 06:05:10 +0900

    blog/Section18-259

---
+++ blog/src/actions/index.js
@@ -0,0 +1,32 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// export const fetchPosts = () => {
+//   return async (dispatch, getState) => {
+//     const res = await jsonPlaceholder.get('/posts');
+
+//     dispatch({
+//       type: 'FETCH_POST',
+//       payload: res,
+//     });
+//   };
+// };
+
+// export const fetchPosts = () => {
+//   return async (dispatch) => {
+//     const res = await jsonPlaceholder.get('/posts');
+
+//     dispatch({
+//       type: 'FETCH_POST',
+//       payload: res,
+//     });
+//   };
+// };
+
+export const fetchPosts = () => async (dispatch) => {
+  const res = await jsonPlaceholder.get('/posts');
+
+  dispatch({
+    type: 'FETCH_POST',
+    payload: res,
+  });
+};
--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,5 +1,11 @@
+import PostList from './PostList';
+
 const App = () => {
-  return <div className="ui container">App</div>;
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
