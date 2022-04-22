f719c315a751574e30357d07fff4838702166302
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-22 12:51:53 +0900

    blog/Section19-285

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,24 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+/**
+ * サマったユーザIDをUserHeader側で取得して何とかするしかないと思っていたけど、
+ * ここでまとめてstoreにしれておけば、UserHeader側で取得できる。
+ */
+export const fetchPostsAndUser =
+  () => async (dispatch, getState /* stateを取得できる */) => {
+    // postをdispatchする
+    await dispatch(fetchPosts());
+
+    // getStateでpostの結果が取得できるので、userIdをサマる
+    const userIdSet = new Set();
+    getState().posts.forEach((post) => userIdSet.add(post.userId));
+
+    // userをdispatchする
+    userIdSet.forEach((userId) => {
+      dispatch(fetchUser(userId));
+    });
+  };
 
 // export const fetchPosts = () => {
 //   return async (dispatch, getState) => {
@@ -46,14 +65,21 @@ export const fetchPosts = () => async (dispatch) => {
 // };
 
 // ■lodash：ユーザを100回fetchしに行くのを防ぐ方法。これだとユーザ情報が変わった場合に対応できない。
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: res.data,
+//   });
+// });
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const res = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: 'FETCH_USER',
     payload: res.data,
   });
-});
+};
