1569228e446c9b654bf3a03ce7727c92af544958
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-22 11:34:44 +0900

    blog/Section19-280

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // export const fetchPosts = () => {
@@ -35,10 +36,24 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  // const res = await jsonPlaceholder.get(`/users/${id}`);
-  // dispatch({
-  //   type: 'FETCH_USER',
-  //   payload: res.data,
-  // });
+// ■問題点：ユーザを100回fetchしに行ってしまう。
+// export const fetchUser = (id) => async (dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: res.data,
+//   });
+// };
+
+// ■lodash：ユーザを100回fetchしに行くのを防ぐ方法。これだとユーザ情報が変わった場合に対応できない。
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: res.data,
+  });
+});
