c8dd37a4d368ba10dc9fede087075ae3a3c21521
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-27 09:39:19 +0900

    streams/client/Section23-328 npm install redux-form --legacy-peer-deps https://blog.npmjs.org/post/626173315965468672/npm-v7-series-beta-release-and-semver-major

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,32 @@
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    const { input, label } = formProps;
+
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <Field name="title" label="Enter Title" component={this.renderInput} />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
